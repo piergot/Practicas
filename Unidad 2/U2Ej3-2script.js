45c95f844719d547f10218e0a93b4276c9d6d5b8
ciclos <ciclos>
2016-09-22 15:17:55 +0200

    Ejericios

---
+++ Unidad 2/U2Ej3-2script.js
@@ -0,0 +1,7 @@
+var s = "Las rosas son rojas \n\t El mar es azul \n\t\t No sé rimar";
+console.log(s);
+var t = "\nLas rosas no son rojas \n\t El mar no es azul \n\t\t Sí sé rimar";
+console.log(t);
+var u = prompt('Introducir texto');
+s = u;
+t = u;
---
+++ Unidad 2/U2Ej3script.js
@@ -0,0 +1,4 @@
+var s = "Las rosas son rojas \n\t El mar es azul \n\t\t No sé rimar";
+console.log(s);
+var t = "\nLas rosas no son rojas \n\t El mar no es azul \n\t\t Sí sé rimar";
+console.log(t);
